e476d6b00c03381f070ea93c84523adc95ba9c25
Rafael dos Santos <p.4ever@gmail.com>
2018-11-16 09:41:19 -0300

    Initial commit

---
+++ AspnetCore.DisasterRecovery/Controllers/LocationController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspnetCore.DisasterRecovery.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var tz = TimeZoneInfo.Local;
+            return Ok(new {
+                tz.Id,
+                tz.DisplayName,
+                time = DateTime.Now.ToUniversalTime(),
+                utc = DateTime.UtcNow.ToUniversalTime()
+            });
+        }
+    }
+}
